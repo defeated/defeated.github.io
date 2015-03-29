ba49237a902100d1ade60027fd2096a897e84e3f
eddie cianci <defeated2k4@gmail.com>
2015-03-29 00:06:34 -0400

    extract tagline rotation into class

--- src/site.js
+++ src/site.js
@@ -1,31 +1,48 @@
 let taglines = [
+  "is having a good hair day",
   "is a humble ruby typist",
   "loves plaid",
   "talks with his hands",
   "doesn't hate javascript",
   "trims his beard like a bonsai",
-  "is having a good hair day",
 ]
 
-let timer, t = 0;
-
-let rotator = () => {
-  let text = taglines[t]
-  let max = taglines.length
-
-  if(t == max) {
-    t = 0
-    clearInterval(timer)
-  }
-  else {
-    t++
+class Rotator {
+  constructor(items, selector) {
+    this.items    = items
+    this.max      = items.length - 1
+    this.index    = 0
+    this.$element = $(selector)
   }
 
-  $('.tagline').fadeOut(function(){
-    $(this).text(text).fadeIn()
-  })
+  start(duration = 5000) {
+    this.timer = setInterval(this.rotate.bind(this), duration)
+  }
+
+  stop() {
+    clearInterval(this.timer)
+  }
+
+  next() {
+    if(this.index == this.max) {
+      return this.stop()
+    }
+    this.index++
+  }
+
+  rotate() {
+    this.$element.fadeOut(() => {
+      this.$element.text(this.tagline).fadeIn()
+    })
+    this.next()
+  }
+
+  get tagline() {
+    return this.items[this.index]
+  }
 }
 
-jQuery(() => {
-  setInterval(rotator, 7500)
+jQuery(function(){
+  let rotator = new Rotator(taglines, '.tagline')
+  rotator.start()
 })
